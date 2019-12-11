6265e25a2aa8ce245bf1cb61cc3d2b1b52171840
Yuriy work-pc <transpmailbox@gmail.com>
2019-12-11 17:56:52 +0200

    Run homepage without any features

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+// import TestComponent from './components/TestComponent';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <dif>{{name}}</dif>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TestComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
